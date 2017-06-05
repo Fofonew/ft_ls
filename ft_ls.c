5aaa8d9d3e7ff000fbd9f883330fa14a9f1d02bb
Dimitri Oriol <doriol@student.42.fr>
2017-06-05 23:00:03 +0200

    01

--- ft_ls.c
+++ ft_ls.c
@@ -6,7 +6,7 @@
 /*   By: fofow <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/04 09:16:35 by fofow             #+#    #+#             */
-/*   Updated: 2017/06/04 10:10:52 by fofow            ###   ########.fr       */
+/*   Updated: 2017/06/05 22:59:43 by fofow            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 #include <sys/dir.h>
 #include <stdio.h>
 
-void	ft_ls(char *name)
+void	show_content(char *dir_name)
 {
-	struct dirent *dirent;
-	DIR *dir;
-	dir = opendir(name);
+	struct dirent	*dirent;
+	DIR				*dir;
+	dir = opendir(dir_name);
 	while (1)
 	{
 		dirent = readdir(dir);
@@ -30,11 +30,32 @@ void	ft_ls(char *name)
 	closedir(dir);
 }
 
+void	recursive_check(char *name)
+{
+	struct dirent	*dirent;
+	DIR				*dir;
+	show_content(name);
+	dir = opendir(name);
+	while (1)
+	{
+		dirent = readdir(dir);
+		if (dirent == NULL)
+			break;
+		if (dirent->d_type == 4)
+		{
+			printf("a");
+			show_content(dirent->d_name);
+			closedir(dir);
+		}
+	}
+}
+
 int		main(int a, char **v)
 {
-	if (a == 1)
-		ft_ls(".");
-	else if (a == 2)
-		ft_ls(v[1]);
-	return 0;
+	//if (a == 1)
+		//show_content(".");
+	//else if (a == 2)
+		//show_content(v[1]);
+	recursive_check(".");
+	return (0);
 }
